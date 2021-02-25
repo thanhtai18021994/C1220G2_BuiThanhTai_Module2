bf01e6926926a241f1ff10d6d3a2ce45584693da
thanhtai18021994 <taibui18021994@gmail.com>
2021-02-25 22:15:50 +0700

    java colection framwork

---
+++ src/_08_cleancode_refactoring/excercise/RefactoringTenisGame.java
@@ -0,0 +1,8 @@
+package _08_cleancode_refactoring.excercise;
+
+public class RefactoringTenisGame {
+    public static String getScore(String player1, String player2, int score1, int score2) {
+
+        return "A";
+    }
+}
---
+++ src/_09_dsa_list/excercise/excercise1/MyListTest.java
@@ -0,0 +1,5 @@
+package _09_dsa_list.excercise.excercise1;
+
+public class MyListTest {
+    
+}
